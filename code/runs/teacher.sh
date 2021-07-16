13a8f6a9417219d39f50c7138903a40b23fe337c
Nikunj-Gupta <Nikunj.Gupta@iiitb.org>
2021-07-17 03:00:18 +0530

    removed bug

--- code/runs/teacher.sh
+++ code/runs/teacher.sh
@@ -11,4 +11,4 @@ module load cuda cudnn
 module load python/3.7
 source ../../../venvs/tsrl/bin/activate 
 
-python main.py --env-key BOX2D-LunarLander --seed 10 --config-set 1000 
+python ../main.py --env-key BOX2D-LunarLander --seed 10 --config-set 1000 
